b0cdc9a36f9194df1a0cd6545e0caf3b976f4b92
Anuj Khare <khareanuj18@gmail.com>
2015-04-13 14:55:39 +0530

    final update

--- reserve.php
+++ reserve.php
@@ -15,6 +15,37 @@
 #
 #		die('Error: ' . mysql_error());
 #			
+	$show_db = $_GET['show_db'];
+	if ($show_db == True) {
+
+#		// sending query
+#		$result = mysql_query("SELECT * FROM new_table1");
+#		if (!$result) {
+#		    die("Query to show fields from table failed");
+#		}
+#		
+#		$fields_num = mysql_num_fields($result);
+#		
+#		echo "<h1>Table: {$table}</h1>";
+#		echo "<table border='1'><tr>";
+#		// printing table headers
+#		for($i=0; $i<$fields_num; $i++)
+#		{
+#		    $field = mysql_fetch_field($result);
+#		    echo "<td>{$field->name}</td>";
+#		}
+#		echo "</tr>\n";
+#		while($row = mysql_fetch_row($result))
+#		{
+#		    echo "<tr>";
+#		
+#		    foreach($row as $cell)
+#		        echo "<td>$cell</td>";
+#		
+#		    echo "</tr>\n";
+#		}
+#		mysql_free_result($result);
+	}
 ?>
 
 <script language="javascript">
